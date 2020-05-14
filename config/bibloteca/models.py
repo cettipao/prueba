82c589a672e46c432291f52afd5f66839722ebe9
cettipao <cettipao@live.com.ar>
2020-05-14 17:27:06 -0300

    Creamos Autor Y libro

--- config/bibloteca/models.py
+++ config/bibloteca/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length = 20)
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length = 20)
+    editorial = models.CharField(max_length = 20)
+    paginas = models.IntegerField()
+    autor = models.ForeignKey(
+        'Autor',
+        on_delete=models.CASCADE,
+        null = False,
+    )
+
