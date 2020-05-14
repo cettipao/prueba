30a7024b0192ef0c756dd8342adc752a49730aa8
cettipao <cettipao@live.com.ar>
2020-05-14 17:47:29 -0300

    Clase Ejemplar y migratons

--- config/bibloteca/models.py
+++ config/bibloteca/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Autor(models.Model):
     nombre = models.CharField(max_length = 20)
+    def __str__(self):
+        return str(self.nombre)
+
 
 class Libro(models.Model):
     titulo = models.CharField(max_length = 20)
@@ -14,6 +17,9 @@ class Libro(models.Model):
         on_delete=models.CASCADE,
         null = False,
     )
+    def __str__(self):
+        return str(self.titulo)
+
 
 class Ejemplar(models.Model):
     localizacion = models.CharField(max_length = 20)
@@ -22,4 +28,14 @@ class Ejemplar(models.Model):
         on_delete=models.CASCADE,
         null = False,
     )
+    def __str__(self):
+        return str(self.localizacion + " " + self.libro.nombre)
 
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length = 30,null=False)
+    telefono = models.CharField(max_length = 10,null=False)
+    direccion = models.CharField(max_length = 30,null=False)
+    ejemplares = models.ManyToManyField(Ejemplar)
+    def __str__(self):
+        return str(self.nombre)
